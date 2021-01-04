f34755237e5af8f8adb96e1c4ce4ac762b762e00
y1z <whyis2cool@gmail.com>
2021-01-04 11:46:44 -0600

    defined what a node is.

--- util/arrayList.h
+++ util/arrayList.h
@@ -1,2 +1,29 @@
 #pragma once
+#include <bitset>
+#include <array>
+#include <cstdint>
 
+
+
+
+template <class type, uint16_t arraySize>
+class arrayListTemplate 
+{
+public:
+
+  /** @brief defines a node for the array list */
+  struct node 
+  {
+    /** @brief contains the individual elements. */
+    std::array<type,arraySize> m_elements;
+
+    /** @brief zero means a index is free, 1 means it's occupied. */
+    std::bitset<arraySize> m_elementTracker;
+  };
+  
+};
+
+
+
+template<class type>
+using arrayList = arrayListTemplate<type,16u>;
