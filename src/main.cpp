05921ecb3bb3a428dc5700e07abdccfee06bf104
y1z <whyis2cool@gmail.com>
2021-01-04 11:30:40 -0600

    Starting work on "arrayList" link list of arrays.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,4 @@
-#include <iostream>
-#include <climits>
-#include "util/enCircularLinkList.h"
-#include "include/operator_new_overloads.h"
-
-
-
 
 int main()
 {
-  std::cout << "memory stats before test\n\n";
-  printMemoryStats();
-  {
-    enCircularLinkList<std::string> intList;
-    intList.addNode("afdsaf");
-    intList.addNode("dafdsaf");
-    for( int32_t i = 0; i < 10; ++i )
-    {
-      const char randomChar = std::rand() % std::numeric_limits<char>::max();
-      const std::string randomString(10, randomChar);
-      intList.addNode(randomString);
-    }
-    std::cout << "\n\nmemory stats after adding all the elements to the list\n\n";
-    printMemoryStats();
-  }
-  std::cout << "\n\n memory stats after destruction of circular-link-list\n\n";
-  printMemoryStats();
-
 }
