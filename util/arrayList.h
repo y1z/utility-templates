3102950ff586e1f61911649779f26de98b867d89
y1z <whyis2cool@gmail.com>
2021-01-05 05:31:04 -0600

    Added more comments to 'arrayListTemplate'.

--- util/arrayList.h
+++ util/arrayList.h
@@ -6,7 +6,12 @@
 
 
 
-template <class type, uint16_t arraySize>
+/**
+ * @class arrayListTemplate : Is a link list but every node contains a array of 
+ * elements instead of individual elements
+ */
+template <class type, 
+  uint16_t arraySize>
 class arrayListTemplate 
 {
 public:
@@ -14,12 +19,31 @@ public:
   /** @brief defines a node for the array list */
   struct node 
   {
+    /** @brief a pointer to the next node.*/
+    node* m_next = nullptr;
+
     /** @brief contains the individual elements. */
     std::array<type,arraySize> m_elements;
 
     /** @brief zero means a index is free, 1 means it's occupied. */
-    std::bitset<arraySize> m_elementTracker;
+    std::bitset<arraySize> m_indexTracker;
   };
+
+public:
+  bool
+  init(const type& element)
+  {
+    if( nullptr == m_firstNode )
+    {
+      m_firstNode = new node();
+      m_firstNode->addElem( );
+    }
+    else
+    {
+      return false;
+    }
+    
+  }
 
 private:
   node* m_firstNode = nullptr;
